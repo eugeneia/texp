9a02b5fc48cf9dbea20e3314fd29a2b9f8db9f65
Max Rottenkolber <max@mr.gy>
2013-09-11 22:47:08 +0200

    Minor documentation changes.

--- texp.lisp
+++ texp.lisp
@@ -2,7 +2,7 @@
 
 (defpackage texp
   (:documentation
-   "DSL for outputting TeX expressions using S-expressions.")
+   "DSL for outputting _TeX_ expressions using s-expressions.")
   (:use :cl
 	:named-readtables)
   (:export :*escape-table*
@@ -32,7 +32,7 @@
     (#\| . "{\\tt\\char`|\\/}")
     (#\< . "{\\tt\\char`<\\/}")
     (#\> . "{\\tt\\char`>\\/}"))
-  "TeX special characters to quoted equivalent mapping.")
+  "_TeX_ special characters to quoted equivalent mapping.")
 
 (defun escape-p (character)
   "Predicate to test if CHARACTER needs to be escaped."
@@ -63,6 +63,9 @@ characters."
 
 (defparameter {} nil
   "Special form symbol for wrapping expressions with curly braces.")
+
+(defparameter syntax nil
+  "Readtable for braces and brackets syntax.")
 
 (defun symbol-identifier (symbol)
   "Return identifier string for SYMBOL."
@@ -101,7 +104,7 @@ characters."
 	  (values)))
 
 (defun compile-expression (expression)
-  "Compile TeX EXPRESSION."
+  "Compile _TeX_ EXPRESSION."
   (etypecase expression
     (string `(write-string ,expression))
     (symbol `(write-string ,(symbol-identifier expression)))
@@ -114,13 +117,13 @@ characters."
 	    (otherwise (apply #'compile-macro-call expression))))))
 
 (defun compile-expressions (expressions)
-  "Compile TeX EXPRESSIONS."
+  "Compile _TeX_ EXPRESSIONS."
   (loop for expression in expressions
      collect (compile-expression expression)))
 
 (defmacro tex (&rest expressions)
-  "Print compiled TeX EXPRESSIONS. Strings and numbers are printed as is
-and symbols are printed in lower case. The {(BR)} special form is
+  "Print compiled _TeX_ EXPRESSIONS. Strings and numbers are printed as
+is and symbols are printed in lower case. The {(BR)} special form is
 converted to two newlines (e.g. paragraph seperator).  The
 {($ FORM)} special form will evaluate FORM and print its result if it is
 a string (e.g. interpolation). Other compound forms (e.g.
@@ -139,8 +142,8 @@ and {\\{&rest EXPRESSIONS\\}}."
   (subseq "#1#2#3#4#5#6#7#8#9" 0 (* n 2)))
 
 (defun compile-parameters (parameters)
-  "Returns a map of PARAMETERS to TeX macro parameter pointers of the form
-'#<N>' and a parameter string for TeX's \\\\def."
+  "Returns a map of PARAMETERS to _TeX_ macro parameter pointers of the form
+'#<N>' and a parameter string for _TeX_'s \\\\def."
   (let ((n (length parameters)))
     (when (> n 9)
       (error "TeX macros support up to nine parameters only."))
@@ -151,9 +154,9 @@ and {\\{&rest EXPRESSIONS\\}}."
      (make-parameter-string n))))
 
 (defmacro deftex (name parameters &body body)
-  "Define a TeX macro with NAME, PARAMETERS and BODY. NAME must be a
+  "Define a _TeX_ macro with NAME, PARAMETERS and BODY. NAME must be a
 symbol or a string. If it is a symbol it will be printed in lower case.
-PARAMETERS must be a list of symbols which will be bound to TeX parameter
+PARAMETERS must be a list of symbols which will be bound to _TeX_ parameter
 identifiers inside BODY. The expressions in BODY will be printed as if by
 TEX and the interpolation form can be used to reference PARAMETERS."
   (multiple-value-bind (pointer-map parameter-string)
